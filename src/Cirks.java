73dbe6370c83e46a3579938b3b3bf5236ee64c1d
Galiņa <nora.galina@sk.lvt.lv>
2023-03-09 12:41:31 +0200

    Integrēta iespēja kauliņu mest vairākas reizes

--- src/Cirks.java
+++ src/Cirks.java
@@ -1,14 +1,17 @@
 import java.util.Random;
 
 public class Cirks {
-	static void mestKaulinu() {
+	static void mestKaulinu(int reizes) {
 		int skaitlis;
 		Random rand = new Random();
+		
+		for(int i=1; i<=reizes; i++) {
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		}
 	}
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(4);
 	}
 
 }
