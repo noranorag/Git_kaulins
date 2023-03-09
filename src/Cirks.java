f7d4c9a86ca39cb6fe96d4eeb0838d327f88e41d
Galiņa <nora.galina@sk.lvt.lv>
2023-03-09 12:56:24 +0200

    Nodrošināta iespēja ievadīt metienu skaitu

--- src/Cirks.java
+++ src/Cirks.java
@@ -1,11 +1,18 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Cirks {
 	public static void main(String[] args) {
-		int skaitlis;
+		int skaitlis, reizes;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kauliòu?");
+		reizes = scan.nextInt();
 		Random rand = new Random();
+		
+		for(int i=1; i<=reizes; i++) {
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		}
 	}
 
 }
