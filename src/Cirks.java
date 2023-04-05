d31cb5da9242550554ef4d8a23c1b6fb1a62fbe9
Galina <nora.galina@sk.lvt.lv>
2023-04-05 09:56:23 +0300

    Papildinats ar lielaka skaitla noteiksanu

--- src/Cirks.java
+++ src/Cirks.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 
 public class Cirks {
 	static void mestKaulinu(int reizes) {
-		int skaitlis;
+		int skaitlis, lielakais = 0;
 		Random rand = new Random();
 		
 		for(int i=1; i<=reizes; i++) {
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		if(lielakais<skaitlis)
+			lielakais = skaitlis;
 		}
+		System.out.println("Lielâkais skaitlis no metieniem bija: "+lielakais);
 }
 	public static void main(String[] args) {
 		int reizes;
